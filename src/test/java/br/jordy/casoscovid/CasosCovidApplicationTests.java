07f5d2091f36163deafeda3966f9d7ca5e90f958
sesap <sesap@imd.local>
2021-06-23 17:25:50 -0300

    Start do projeto

---
+++ src/test/java/br/jordy/casoscovid/CasosCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jordy.casoscovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasosCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
